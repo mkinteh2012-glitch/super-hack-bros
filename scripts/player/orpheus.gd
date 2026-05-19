9436a08c73e2c6d8012d0571a98fad6d5972bb85
mkinteh2012-glitch <mkinteh2012@gmail.com>
2026-05-18 21:31:32 -0700

    Orphues has head smash

--- scripts/player/orpheus.gd
+++ scripts/player/orpheus.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
+# --- Node References ---
 @onready var sprite = $AnimatedSprite2D
 @onready var hitbox = $CollisionPolygon2D
 @onready var hurtbox = $Hurtbox 
+
+# Punch Nodes
 @onready var punch_box = $PunchBox
 @onready var punch_hitbox = $PunchBox/PunchHitbox
+
+# Skull Bash Nodes
+@onready var skull_box = $SkullBox
+@onready var skull_hitbox = $SkullBox/SkullBox 
 
 @export_enum("Player 1:1", "Player 2:2") var player_id: int = 1
 
@@ -13,86 +20,76 @@ extends CharacterBody2D
 @export var acceleration: float = 300.0    
 @export var friction: float = 1600.0       
 
-# --- Combat & Scaling Physics ---
+# --- Physics Constants ---
 @export var Jump_Velocity: float = -650.0
 @export var base_gravity_multiplier: float = 2.0
 @export var fall_gravity_multiplier: float = 3.5
 @export var knockback_resistance: float = 650.0 
+@export var lunge_speed: float = 180.0     
 
-# --- Combat & Combo Status Pool ---
+# --- Skull Bash Custom Launch Velocity ---
+@export var skull_bash_velocity_y: float = -850.0 
+
+# --- Health Pool ---
 var damage_percent: float = 0.0 
-var combo_count: int = 0             
-var combo_reset_timer: float = 0.0   
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_attacking: bool = false
 var holding_last_frame: bool = false
 var is_in_knockback: bool = false
 
+# --- Special State Flags ---
+var is_skull_bashing: bool = false 
+var is_charging_bash: bool = false  
+
 var input_left: String = ""
 var input_right: String = ""
 var input_up: String = ""
 var input_attack: String = ""
+var input_special: String = "" 
 
 func _ready():
 	sprite.animation_finished.connect(_on_animation_finished)
 	sprite.frame_changed.connect(_on_sprite_frame_changed)
 	
-	if punch_box:
-		punch_box.area_entered.connect(_on_punch_box_area_entered)
+	if punch_box: punch_box.area_entered.connect(_on_punch_box_area_entered)
+	if skull_box: skull_box.area_entered.connect(_on_skull_box_area_entered)
 
+	# --- Dynamic Input Strings Configuration ---
 	input_left = "p%d_left" % player_id
 	input_right = "p%d_right" % player_id
 	input_up = "p%d_up" % player_id
 	input_attack = "p%d_attack" % player_id
+	input_special = "p%d_special" % player_id 
 	
-	if punch_hitbox:
-		punch_hitbox.disabled = true
+	if punch_hitbox: punch_hitbox.disabled = true
+	if skull_hitbox: skull_hitbox.disabled = true
 
-	# --- Automatic Layer and Mask Setup ---
-	set_collision_layer_value(1, true)
-	set_collision_layer_value(2, true)
-	set_collision_mask_value(1, true)
-	set_collision_mask_value(2, true)
-	
-	if hurtbox:
-		hurtbox.set_collision_layer_value(1, true)
-		hurtbox.set_collision_layer_value(2, true)
-		hurtbox.set_collision_mask_value(1, true)
-		hurtbox.set_collision_mask_value(2, true)
-		
-	if punch_box:
-		punch_box.set_collision_layer_value(1, true)
-		punch_box.set_collision_layer_value(2, true)
-		punch_box.set_collision_mask_value(1, true)
-		punch_box.set_collision_mask_value(2, true)
+	_setup_collision_layers()
 
 func _physics_process(delta):
 	queue_redraw()
 
-	# Manage combo window decline decay over time
-	if combo_count > 0 and not is_attacking:
-		combo_reset_timer -= delta
-		if combo_reset_timer <= 0:
-			combo_count = 0
-			print("[COMBO] Window expired. Reset to 0.")
-
-	# Handle Constant Gravity
+	# --- Gravity Processing ---
 	if not is_on_floor():
 		if velocity.y > 0:
 			velocity.y += gravity * fall_gravity_multiplier * delta
+			if is_skull_bashing:
+				end_skull_bash_state()
 		else:
 			velocity.y += gravity * base_gravity_multiplier * delta
 		
-	# --- Fast Pace Input Buffer & Aerial Trigger Check ---
-	# FIX: Removed 'is_on_floor()' constraint so it triggers mid-air!
-	if Input.is_action_just_pressed(input_attack) and not is_in_knockback:
-		if is_attacking:
-			if combo_count < 3:
-				trigger_combo_strike()
-		else:
-			trigger_combo_strike()
+	# --- Input Checks: Dynamic Move Separation ---
+	
+	# FIX: Skull Bash requires BOTH input_special pressed AND input_up held down
+	if Input.is_action_just_pressed(input_special) and Input.is_action_pressed(input_up) and not is_in_knockback and not is_attacking:
+		trigger_skull_bash_charge()
+			
+	# Regular Combo attack configuration
+	elif Input.is_action_just_pressed(input_attack) and not is_in_knockback and not is_attacking:
+		trigger_combo_strike()
 
+	# Normal Jump Mechanics
 	if Input.is_action_just_pressed(input_up) and is_on_floor() and not is_attacking and not is_in_knockback:
 		velocity.y = Jump_Velocity
 	
@@ -102,10 +99,7 @@ func _physics_process(delta):
 		if direction != 0:
 			velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
 			sprite.flip_h = (direction < 0)
-			
-			if hitbox: hitbox.scale.x = -1.0 if direction < 0 else 1.0
-			if punch_box: punch_box.scale.x = -1.0 if direction < 0 else 1.0
-			if hurtbox: hurtbox.scale.x = -1.0 if direction < 0 else 1.0
+			_flip_hitbox_directions(direction < 0)
 		else:
 			velocity.x = move_toward(velocity.x, 0.0, friction * delta)
 	
@@ -114,34 +108,81 @@ func _physics_process(delta):
 		if is_on_floor() and abs(velocity.x) < 30.0:
 			is_in_knockback = false
 	else:
-		# Ground attacks stop you completely, Air attacks let you glide through space using momentum
-		if is_on_floor():
-			velocity.x = move_toward(velocity.x, 0.0, friction * delta)
+		if is_charging_bash:
+			velocity.x = move_toward(velocity.x, 0.0, friction * 0.2 * delta)
+		elif is_skull_bashing:
+			velocity.x = 0.0
 		else:
-			# Minor air resistance while swinging mid-air
-			velocity.x = move_toward(velocity.x, 0.0, friction * 0.15 * delta)
+			velocity.x = move_toward(velocity.x, 0.0, friction * 0.7 * delta)
 		
 	move_and_slide()
 	queue_animations(direction)
 
-# Progressive strike command handler
+# --- Attack Activation States ---
+
 func trigger_combo_strike():
-	combo_count += 1
-	print("[COMBO] Executing Hit #", combo_count)
-	
 	is_attacking = true
 	holding_last_frame = false
 	
-	# FIX: Only zero out speed if we are running on the ground!
-	if is_on_floor():
-		velocity.x = 0.0
+	var lunge_direction = -1.0 if sprite.flip_h else 1.0
+	velocity.x = lunge_direction * lunge_speed
 	
-	if punch_hitbox:
-		punch_hitbox.set_deferred("disabled", true) 
+	if punch_hitbox: punch_hitbox.set_deferred("disabled", true) 
+	if skull_hitbox: skull_hitbox.set_deferred("disabled", true)
 		
 	sprite.stop()
 	sprite.frame = 0
 	sprite.play("punch")
+
+func trigger_skull_bash_charge():
+	is_attacking = true
+	is_charging_bash = true
+	is_skull_bashing = false
+	holding_last_frame = false
+	
+	if punch_hitbox: punch_hitbox.set_deferred("disabled", true)
+	if skull_hitbox: skull_hitbox.set_deferred("disabled", true)
+		
+	sprite.stop()
+	sprite.frame = 0
+	sprite.play("skull_bash")
+
+func release_skull_bash_launch():
+	is_charging_bash = false
+	is_skull_bashing = true
+	
+	velocity.y = skull_bash_velocity_y
+	velocity.x = 0.0 
+	
+	if skull_hitbox:
+		skull_hitbox.set_deferred("disabled", false)
+
+func end_skull_bash_state():
+	is_attacking = false
+	is_skull_bashing = false
+	is_charging_bash = false
+	holding_last_frame = false
+	
+	if skull_hitbox:
+		skull_hitbox.set_deferred("disabled", true)
+		
+
+		if is_on_floor(): sprite.play("idle")
+		else: sprite.play("default")
+
+# --- Frame & Animation Management ---
+
+func _on_sprite_frame_changed():
+	if is_charging_bash and sprite.animation == "skull_bash":
+		var total_frames = sprite.sprite_frames.get_frame_count("skull_bash")
+		if sprite.frame == total_frames - 1:
+			release_skull_bash_launch()
+			
+	elif is_attacking and sprite.animation == "punch":
+		var total_frames = sprite.sprite_frames.get_frame_count("punch")
+		if sprite.frame == total_frames - 1:
+			if punch_hitbox:
+				punch_hitbox.set_deferred("disabled", false)
 
 func queue_animations(direction: float):
 	if is_in_knockback:
@@ -149,99 +190,92 @@ func queue_animations(direction: float):
 		return
 
 	if is_attacking:
-		if holding_last_frame:
-			sprite.frame = sprite.sprite_frames.get_frame_count("punch") - 1
+		if is_skull_bashing or holding_last_frame:
+			var total_frames = sprite.sprite_frames.get_frame_count(sprite.animation)
+			sprite.frame = total_frames - 1
 			sprite.pause()
 		return
 
 	if is_on_floor():
-		if direction == 0:
-			sprite.play("idle")
-		else:
-			sprite.play("default")
+		if direction == 0: sprite.play("idle")
+		else: sprite.play("default")
 	else:
 		sprite.play("default")
-
-func _on_sprite_frame_changed():
-	if is_attacking and sprite.animation == "punch":
-		var total_frames = sprite.sprite_frames.get_frame_count("punch")
-		var current_frame = sprite.frame
-		
-		if current_frame == total_frames - 1:
-			if punch_hitbox and punch_hitbox.disabled:
-				punch_hitbox.set_deferred("disabled", false)
-
-# --- Combat Logic ---
-
-func _on_punch_box_area_entered(area):
-	if area.name == "Hurtbox":
-		var opponent = area.get_parent()
-		if opponent != self and opponent.has_method("take_damage_and_knockback"):
-			var knockback_dir = -1.0 if sprite.flip_h else 1.0
-			
-			if knockback_dir == 0:
-				knockback_dir = -1.0 if Input.is_action_pressed(input_left) else 1.0
-				
-			opponent.take_damage_and_knockback(5.0, knockback_dir, combo_count)
-
-func take_damage_and_knockback(damage_amount: float, hit_direction: float, attacker_combo_tier: int = 1):
-	damage_percent += damage_amount
-	print("[DAMAGE] Player ", player_id, " hit! Total Damage: ", damage_percent, "%")
-	
-	if hit_direction == 0:
-		hit_direction = 1.0
-		
-	var launch_chance = clamp(damage_percent / 150.0, 0.0, 0.95)
-	
-	if attacker_combo_tier >= 3:
-		launch_chance += 0.45 
-		print("[COMBO FINISHER] Boosted Launch Chance Evaluated: ", launch_chance * 100, "%")
-		
-	var random_roll = randf()
-	var launch_x = 0.0
-	var launch_y = 0.0
-	
-	if random_roll < launch_chance:
-		# --- CRITICAL BLAST LAUNCH ---
-		var power_multiplier = 1.4 if attacker_combo_tier >= 3 else 1.0
-		print("[COMBAT] CRITICAL LAUNCH!")
-		launch_x = (280.0 + (damage_percent * 4.0)) * hit_direction * power_multiplier
-		launch_y = (-240.0 - (damage_percent * 4.5)) * power_multiplier
-	else:
-		# --- NORMAL FLINCH ---
-		print("[COMBAT] Standard Flinch.")
-		launch_x = (160.0 + (damage_percent * 1.5)) * hit_direction
-		launch_y = -140.0 - (damage_percent * 0.8)
-
-	velocity = Vector2(launch_x, launch_y)
-	is_in_knockback = true
-	is_attacking = false 
-	holding_last_frame = false
-	combo_count = 0 
-	
-	if punch_hitbox:
-		punch_hitbox.set_deferred("disabled", true)
 
 func _on_animation_finished():
 	if sprite.animation == "punch":
 		holding_last_frame = true
+		if punch_hitbox: punch_hitbox.set_deferred("disabled", true)
 		
-		if punch_hitbox:
-			punch_hitbox.set_deferred("disabled", true)
-		
-		get_tree().create_timer(0.03).timeout.connect(func():
+		get_tree().create_timer(0.04).timeout.connect(func():
 			if holding_last_frame:
 				is_attacking = false
 				holding_last_frame = false
-				combo_reset_timer = 0.45 
-				if combo_count >= 3:
-					combo_count = 0 
 		)
 
-func _draw():
-	var debug_color = Color.GREEN
-	if is_in_knockback:
-		debug_color = Color.BLUE
-	elif is_attacking:
-		debug_color = Color.YELLOW if holding_last_frame else Color.RED
-	draw_circle(Vector2(0, -45), 5.0, debug_color)
+# --- Hit Detection Loops ---
+
+func _on_punch_box_area_entered(area):
+	if area.name == "Hurtbox":
+		var opponent = area.get_parent()
+		if opponent != self and opponent.has_method("universal_take_damage"):
+			var knockback_dir = -1.0 if sprite.flip_h else 1.0
+			var random_damage = randi_range(3, 7)
+			opponent.universal_take_damage(random_damage, knockback_dir, 160.0, -140.0, 1.2)
+
+func _on_skull_box_area_entered(area):
+	if area.name == "Hurtbox":
+		var opponent = area.get_parent()
+		if opponent != self and opponent.has_method("universal_take_damage"):
+			var knockback_dir = -1.0 if sprite.flip_h else 1.0
+			opponent.universal_take_damage(14.0, knockback_dir, 0.0, -380.0, 4.0)
+			if skull_hitbox:
+				skull_hitbox.set_deferred("disabled", true)
+
+# --- Universal Receiver Damage Function ---
+func universal_take_damage(damage: float, hit_dir: float, base_kb_x: float, base_kb_y: float, kb_scaling: float):
+	damage_percent += damage
+	print("[HIT] Player ", player_id, " took ", damage, "%. Total: ", damage_percent, "%")
+	
+	if hit_dir == 0: hit_dir = 1.0
+	
+	var launch_chance = clamp(damage_percent / 150.0, 0.0, 0.95)
+	var random_roll = randf()
+	
+	var final_x = 0.0
+	var final_y = 0.0
+	
+	if random_roll < launch_chance:
+		print("[COMBAT] CRITICAL LAUNCH TRIGGERED!")
+		final_x = (base_kb_x + (damage_percent * kb_scaling)) * hit_dir * 1.3
+		final_y = (base_kb_y - (damage_percent * kb_scaling)) * 1.3
+	else:
+		print("[COMBAT] Standard Flinch.")
+		final_x = base_kb_x * hit_dir
+		final_y = base_kb_y * 0.75
+
+	velocity = Vector2(final_x, final_y)
+	is_in_knockback = true
+	is_attacking = false 
+	is_skull_bashing = false
+	is_charging_bash = false
+	holding_last_frame = false
+	
+	if punch_hitbox: punch_hitbox.set_deferred("disabled", true)
+	if skull_hitbox: skull_hitbox.set_deferred("disabled", true)
+
+# --- Helpers ---
+func _flip_hitbox_directions(is_flipped: bool):
+	var flip_scale = -1.0 if is_flipped else 1.0
+	if hitbox: hitbox.scale.x = flip_scale
+	if punch_box: punch_box.scale.x = flip_scale
+	if hurtbox: hurtbox.scale.x = flip_scale
+	if skull_box: skull_box.scale.x = flip_scale
+
+func _setup_collision_layers():
+	for i in [1, 2]:
+		set_collision_layer_value(i, true)
+		set_collision_mask_value(i, true)
+		if hurtbox: hurtbox.set_collision_layer_value(i, true); hurtbox.set_collision_mask_value(i, true)
+		if punch_box: punch_box.set_collision_layer_value(i, true); punch_box.set_collision_mask_value(i, true)
+		if skull_box: skull_box.set_collision_layer_value(i, true); skull_box.set_collision_mask_value(i, true)
